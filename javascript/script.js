db621b3fb7a2ed7b3786105e007a7a73f47881f1
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-08-07 15:55:20 -0400

    fixed index.html

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,8 @@
 const calcbtn= document.querySelector(".calculateButton");
 
 function setGrades() {
-	// Define those variables!
+
+	// Define grade inputs.
 		var input1A = parseFloat(document.querySelector(".input1A").value);
 		var input2A = parseFloat(document.querySelector(".input2A").value);
 		var input3A = parseFloat(document.querySelector(".input3A").value);
@@ -9,10 +10,10 @@ function setGrades() {
 		var input5A = parseFloat(document.querySelector(".input5A").value);
 
 
-		//set the weight array.
+		//set the grade array.
 		var grades= [input1A, input2A, input3A, input4A, input5A];
 
-		// Define those variables!
+		// Define weight inputs.
 			var input1B = parseFloat(document.querySelector(".input1B").value);
 			var input2B = parseFloat(document.querySelector(".input2B").value);
 			var input3B = parseFloat(document.querySelector(".input3B").value);
@@ -24,19 +25,20 @@ function setGrades() {
 			var weight= [input1B, input2B, input3B, input4B, input5B];
 
 			//for loop that runs from 0 to length of array.
-			//check if at position blabla it is NaN. if it is, then just add position 0 to current position. if not, then add a counter.
+
 
 			var i;
 			var sum=0;
 
-
-
 			for(i=0; i< weight.length; i++)
 			{
 
-				//divid
+				//divide int
 				let divide=grades[i] / 100;
 
+				console.log(grades);
+
+				//check if at position blabla it is NaN. if it is, then just add position 0 to current position. if not, then add a counter.
 				if (isNaN(weight[i]))
 				{
 						var weightPortion= weight.slice(0,i);
@@ -55,8 +57,8 @@ function setGrades() {
 			};
 
 
-		document.querySelector(".total").value = sum;
+		document.querySelector(".total").value = sum + "%";
 
 	}
 
-	calcbtn.addEventListener("click", setGrades, false); //reset button action listener.
+	calcbtn.addEventListener("click", setGrades, false); //calculate button listener.
