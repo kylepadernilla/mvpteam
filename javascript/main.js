9691d4fe43dec1079fe86e6d193b23aef28fb159
RidaAhmad098 <53270962+RidaAhmad098@users.noreply.github.com>
2019-08-14 10:02:41 -0400

    added form in same page

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+
+
 //adding in Courses
 const courseList = document.querySelector('.courses');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
@@ -42,11 +44,87 @@ const setupCourses = (data) => {
         </div>
         <div class="collapsible-body white">
 
-
         <p>${course.content}</p>
         <a href="../calc.html">Click here to calculate your course mark</a>
 
         </div>
+
+        <div class="collapsible-body white">
+
+        <p class="topheader">
+
+        <span class="inputSec">Input #</span>
+        <span class= "grade">Grade</span>
+        <span class="weight">Weight</span>
+
+        </p>
+
+
+        <!-- Inputs -->
+        <p>
+        <p>Input 1:
+
+        <input type="text" name="input1" class="input1A" value="">
+
+        <input type="text" name="input2" class="input1B" value="">
+        </p>
+
+        </p>
+
+        <p>
+        <p>Input 2:
+
+        <input type="text" name="input1" class="input2A" value="">
+
+        <input type="text" name="input2" class="input2B" value="">
+        </p>
+
+        </p>
+
+        <p>
+        <p>Input 3:
+
+        <input type="text" name="input1" class="input3A" value="">
+
+        <input type="text" name="input2" class="input3B" value="">
+        </p>
+
+        </p>
+
+        <p>
+        <p>Input 4:
+
+        <input type="text" name="input1" class="input4A" value="">
+
+        <input type="text" name="input2" class="input4B" value="">
+        </p>
+
+        </p>
+
+        <p>
+        <p>Input 5:
+
+        <input type="text" name="input1" class="input5A" value="">
+
+        <input type="text" name="input2" class="input5B" value="">
+        </p>
+
+        </p>
+
+        <br>
+        <button type="button" class="calculateButton">Calculate!</button>
+
+        <div class="output">
+          <h3>Your Overall mark is: <input type="text" name="input2" class="total" value=""></h3>
+
+        </div>
+
+        <div class="output">
+          <h3>Your gpa for the course is: <input type="text" name="input2" step="any" class="gpaOutput" value=""></h3>
+
+        </div>
+        </div>
+
 
 
       </li>
@@ -65,6 +143,7 @@ else{
 
 //coursename.innerHTML = `<h2 class="collapsible-header grey lighten-4">${course.title}</h2>`
 
+$.getScript
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
