6361d7710053a1735f7dad35a3cf30794ff36bf7
RidaAhmad098 <53270962+RidaAhmad098@users.noreply.github.com>
2019-08-14 15:38:02 -0400

    confused

--- javascript/coolStuff.js
+++ javascript/coolStuff.js
@@ -1,42 +1,15 @@
-//inputs and grades into database
-
-const grades = document.querySelector(".inputA");
-//const weights = document.querySelectorAll('.inputB');
-/*
-var arr = $("input[name='input1']").map(function() {
-    return this.value;
-}).get();
-
-/*
-var weight = $("input[name='input2']").map(function() {
-    return this.value;
-}).get();
-*/
-//console.log(arr);
-//console.log(weight);
+const input1A = document.querySelector('#inputsList');
 
 
-    grades.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const user = firebase.auth().currentUser;
-    const other = user.currentUser;
-
-    data.collection('users').doc(user.uid).collection('courses').doc(other.uid).collection('grades').add({ //.doc only gets strings.
-      //title: create['title'].value,
-      //content: create['content'].value
-      num: grades['inputA'].value
-    }).then(() => {
-      console.log("hello");
-    });
-  });
-
-
-
-
-    /* close the create modal & reset form
-    const modal = document.querySelector('#modal-create');
-    M.Modal.getInstance(modal).close();
-    create.reset();
-  }).catch(err => {
+input1A.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("hello");
+const user = firebase.auth().currentUser;
+  data.collection('users').doc(user.uid).add({
+    inputOne: input1A['input1A'].value
+  }).then (() => {
+    console.log("hello");
+  }).catch (err =>{
     console.log(err.message);
-  }); */
+  });
+});
