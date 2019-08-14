8060345664df9a42ef2d70a79b3e445d11cafd10
RidaAhmad098 <53270962+RidaAhmad098@users.noreply.github.com>
2019-08-14 14:57:59 -0400

    changed

--- javascript/coolStuff.js
+++ javascript/coolStuff.js
@@ -1,27 +1,42 @@
 //inputs and grades into database
 
-const grades = document.querySelectorAll(".inputA");
+const grades = document.querySelector(".inputA");
 //const weights = document.querySelectorAll('.inputB');
-
+/*
 var arr = $("input[name='input1']").map(function() {
     return this.value;
 }).get();
 
-console.log(arr);
-
 /*
-create.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const user = firebase.auth().currentUser;
-  data.collection('users').doc(user.uid).collection('courses').add({ //.doc only gets strings.
-    title: create['title'].value,
-    content: create['content'].value
-  }).then(() => {
-    // close the create modal & reset form
+var weight = $("input[name='input2']").map(function() {
+    return this.value;
+}).get();
+*/
+//console.log(arr);
+//console.log(weight);
+
+
+    grades.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const user = firebase.auth().currentUser;
+    const other = user.currentUser;
+
+    data.collection('users').doc(user.uid).collection('courses').doc(other.uid).collection('grades').add({ //.doc only gets strings.
+      //title: create['title'].value,
+      //content: create['content'].value
+      num: grades['inputA'].value
+    }).then(() => {
+      console.log("hello");
+    });
+  });
+
+
+
+
+    /* close the create modal & reset form
     const modal = document.querySelector('#modal-create');
     M.Modal.getInstance(modal).close();
     create.reset();
   }).catch(err => {
     console.log(err.message);
-  });
-*/
+  }); */
