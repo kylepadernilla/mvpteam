92a042cbfc74dbd0b2fe322d0f3dcf1c16465520
RidaAhmad098 <53270962+RidaAhmad098@users.noreply.github.com>
2019-08-15 09:46:14 -0400

    fixed stuff

--- javascript/coolStuff.js
+++ javascript/coolStuff.js
@@ -1,33 +1,34 @@
-const input1 = document.querySelector("#inputsList");
+const input1 = document.querySelector("#input1A");
+const button = document.querySelector(".insertButton");
 console.log(input1.value);
 
-var currentUser;
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-      currentUser = user.uid;
-      console.log(currentUser); //this returns my user object
-      window.localStorage.setItem("UID",currentUser);
-  } else {
-      currentUser = "Error"
-      console.log(currentUser); //this returns my user object
-      window.localStorage.setItem("UID",currentUser);
-       alert(" Error in your login code");
-    // No user is signed in.
-  }
-});
-var getuid = window.localStorage.getItem("UID")
-console.log(getuid) // will log the UID
 
+function getUser (){
+  var currentUser;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser = user.uid;
+        console.log(currentUser); //this returns my user object
+        window.localStorage.setItem("UID",currentUser);
+    } else {
+        currentUser = "Error"
+        console.log(currentUser); //this returns my user object
+        window.localStorage.setItem("UID",currentUser);
+         alert(" Error in your login code");
+      // No user is signed in.
+    }
+  });
+  var getuid = window.localStorage.getItem("UID")
+  console.log(getuid) // will log the UID
 
-
-input1.addEventListener("submit", function(e) {
-  e.preventDefault();
-  console.log("hello");
   var ref = data.collection('users').doc(getuid);
   var merge =  ref.set({
-      inputOne: input1['input1A'].value
+      inputOne: input1.value
   }, {merge:true});
 
+  console.log(input1.value);
+}
 
 
-});
+
+button.addEventListener('click', getUser, false);
