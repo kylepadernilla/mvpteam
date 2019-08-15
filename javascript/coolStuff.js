b114cdbb3d31a293c803396981e14429aa7d6961
RidaAhmad098 <53270962+RidaAhmad098@users.noreply.github.com>
2019-08-15 12:38:17 -0400

    INPUT STAYED

--- javascript/coolStuff.js
+++ javascript/coolStuff.js
@@ -1,10 +1,31 @@
-const input1 = document.querySelector("#input1A");
 const button = document.querySelector(".insertButton");
 
-var re;
-initialIn(re);
-console.log(re);
-input1.value = re;
+//INPUT 1
+const input1 = document.querySelector("#input1A");
+
+initialIn();
+function initialIn() {
+  getUser();
+  var getuid = window.localStorage.getItem("UID")
+  console.log(getuid) // will log the UID
+  var ref = data.collection('users').doc(getuid);
+  ref.get().then(function(doc) {
+    if (doc.exists) {
+      var data1 = doc.data();
+      var result = Object.keys(data1).map(function(key) {
+        return data1[key];
+      });
+      var rs = result[0];
+      var a = rs;
+      input1.value = a;
+    } else {
+      console.log("No such doc");
+    }
+  }).catch(function(error) {
+    console.log("Error getting doc", error);
+  });
+}
+
 function getUser (){
   var currentUser;
   firebase.auth().onAuthStateChanged(function(user) {
@@ -34,60 +55,12 @@ function storeUser(){
   console.log(input1.value);
 };
 
-function initialIn(a) {
-  getUser();
-  var getuid = window.localStorage.getItem("UID")
-  console.log(getuid) // will log the UID
-  var ref = data.collection('users').doc(getuid);
-  ref.get().then(function(doc) {
-    if (doc.exists) {
-      var data1 = doc.data();
-      var result = Object.keys(data1).map(function(key) {
-        return data1[key];
-      });
-      console.log(result[0]);
-      console.log(typeof data1);
-      console.log("data:", doc.data());
-      var rs = result[0];
+//END OF INPUT 1
 
-      a += result[0];
-      console.log(a);
-      return a;
-    } else {
-      console.log("No such doc");
-    }
-  }).catch(function(error) {
-    console.log("Error getting doc", error);
-  });
-}
+//INPUT 2
 
 
 
-/*
-//create element and render to input box
-function renderInput (doc){
-  let li = document.createElement('input');
-  let inputOne = document.createElement('ul');
-
-  console.log(input1.appendChild(li));
-  li.setAttribute('date-id', doc.id);
-  inputOne.textContent = doc.data().inputOne;
-
-  li.appendChild(inputOne);
-  input1.appendChild(li);
-};
-
-
-  getUser();
-  var getuid = window.localStorage.getItem("UID");
-  console.log(data.collection('users').doc(getuid));
-  data.collection('users').doc(getuid).get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-      console.log(doc.data());
-      renderInput(doc);
-    });
-  });
-  */
 
 
 
